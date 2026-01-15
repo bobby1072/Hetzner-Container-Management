9f9f99d8907d5c3516dc5059fcc86c38c1e1d624
BobbyThompson <bobbythompson@paypoint.com>
2026-01-15 00:04:37 +0000

    Methopd split

--- src/Hetzner.Container.Management/Hetzner.Container.Management.Services/Extensions/HostApplicationBuilderExtensions.cs
+++ src/Hetzner.Container.Management/Hetzner.Container.Management.Services/Extensions/HostApplicationBuilderExtensions.cs
@@ -16,6 +16,31 @@ public static class HostApplicationBuilderExtensions
 {
     public static IHostApplicationBuilder AddContainerManagementApplication(this IHostApplicationBuilder hostAppBuilder)
     {
+        hostAppBuilder.AddHttClientStuff();
+        
+        return hostAppBuilder;
+    }
+    public static T CheckAndAddSingletonOptions<T>(this IHostApplicationBuilder hostAppBuilder,
+        string? nameofSection = null) where T : class
+    {
+        var sectname = nameofSection ?? typeof(T).Name;
+        
+        var configSection = hostAppBuilder.Configuration.GetSection(sectname);
+
+        if (!configSection.Exists())
+        {
+            throw new ArgumentException(sectname);
+        }
+
+        hostAppBuilder.Services
+            .ConfigureSingletonOptions<T>(configSection);
+        
+        return configSection.Get<T>() ?? throw new ArgumentException(sectname);
+    }
+
+
+    private static IHostApplicationBuilder AddHttClientStuff(this IHostApplicationBuilder hostAppBuilder)
+    {
         var dockerEngineApiSettings = hostAppBuilder.CheckAndAddSingletonOptions<DockerEngineApiSettings>();
         var dockerHubApiSettings = hostAppBuilder.CheckAndAddSingletonOptions<DockerHubApiSettings>();
 
@@ -40,26 +65,7 @@ public static class HostApplicationBuilderExtensions
                     return new NetworkStream(socket, ownsSocket: true);
                 }
             });
-        
-        
+
         return hostAppBuilder;
-    }
-
-    public static T CheckAndAddSingletonOptions<T>(this IHostApplicationBuilder hostAppBuilder,
-        string? nameofSection = null) where T : class
-    {
-        var sectname = nameofSection ?? typeof(T).Name;
-        
-        var configSection = hostAppBuilder.Configuration.GetSection(sectname);
-
-        if (!configSection.Exists())
-        {
-            throw new ArgumentException(sectname);
-        }
-
-        hostAppBuilder.Services
-            .ConfigureSingletonOptions<T>(configSection);
-        
-        return configSection.Get<T>() ?? throw new ArgumentException(sectname);
     }
 }
