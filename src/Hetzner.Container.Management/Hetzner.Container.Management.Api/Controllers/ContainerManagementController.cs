a3e1123b34212f9bfcbbf4858bddeb8115df6a8c
bobby1072 <bobby.thompson1072@gmail.com>
2026-01-18 02:20:04 +0000

    Added controller

---
+++ src/Hetzner.Container.Management/Hetzner.Container.Management.Api/Controllers/ContainerManagementController.cs
@@ -0,0 +1,112 @@
+﻿using System.Net;
+using BT.Common.Api.Helpers.Exceptions;
+using BT.Common.Api.Helpers.Models;
+using Hetzner.Container.Management.Schemas.Infrastructure;
+using Hetzner.Container.Management.Schemas.Input;
+using Hetzner.Container.Management.Services;
+using Hetzner.Container.Management.Services.ContainerOrchestration.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hetzner.Container.Management.Api.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[Route("[controller]")]
+public sealed class ContainerManagementController : ControllerBase
+{
+    private readonly IContainerManagementOperationQueue _containerManagementOperationQueue;
+    private readonly ILogger<ContainerManagementController> _logger;
+
+    public ContainerManagementController(
+        IContainerManagementOperationQueue containerManagementOperationQueue,
+        ILogger<ContainerManagementController> logger
+    )
+    {
+        _containerManagementOperationQueue = containerManagementOperationQueue;
+        _logger = logger;
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IResult> QueueInfrastructureUpdate(
+        [FromBody] InfrastructureComponentUpdateInput[] input,
+        CancellationToken token = default
+    )
+    {
+        try
+        {
+            var queueResult = await _containerManagementOperationQueue.QueueUpdateOperation(
+                input,
+                token
+            );
+
+            return Results.Ok(new WebOutcome<Guid> { Data = queueResult });
+        }
+        catch (ApiException ex)
+        {
+            _logger.Log(
+                ex.LogLevel,
+                "An api exception occured during request with message: {ExMessage}",
+                ex.Message
+            );
+
+            return ex.ToResult();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                ex,
+                "An exception occured during request with message: {Message}",
+                ex.Message
+            );
+
+            return Results.Problem(
+                ApplicationConstants.ExceptionConstants.InternalError,
+                null,
+                (int)HttpStatusCode.InternalServerError
+            );
+        }
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IResult> QueueAndWaitForInfrastructureUpdate(
+        [FromBody] InfrastructureComponentUpdateInput[] input,
+        CancellationToken token = default
+    )
+    {
+        try
+        {
+            var queueResult =
+                await _containerManagementOperationQueue.QueueAndWaitForUpdateOperation(
+                    input,
+                    token
+                );
+
+            return Results.Ok(new WebOutcome<InfrastructureDocument> { Data = queueResult });
+        }
+        catch (ApiException ex)
+        {
+            _logger.Log(
+                ex.LogLevel,
+                "An api exception occured during request with message: {ExMessage}",
+                ex.Message
+            );
+
+            return ex.ToResult();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                ex,
+                "An exception occured during request with message: {Message}",
+                ex.Message
+            );
+
+            return Results.Problem(
+                ApplicationConstants.ExceptionConstants.InternalError,
+                null,
+                (int)HttpStatusCode.InternalServerError
+            );
+        }
+    }
+}
